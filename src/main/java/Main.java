746c0012eb928e4f24989c211748b565d9f726bf
Yrii Parhom <parhomenko.yurij@gmail.com>
2022-05-30 10:52:21 +0300

    ver.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("101", "110"));
+        System.out.println(bins.mult("101", "110"));
+    }
+}
